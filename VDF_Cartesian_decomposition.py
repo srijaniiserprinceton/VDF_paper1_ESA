f45a7e82f93c6582e10f117b1bb072b312df51c3
srijaniiserprinceton <sbdas@princeton.edu>
2024-08-21 00:41:22 -0400

    Completely polished script VDF_Cartesian_decomposition.py

--- VDF_Cartesian_decomposition.py
+++ VDF_Cartesian_decomposition.py
@@ -13,6 +13,12 @@ eng.addpath(s, nargout=0)
 # adding custom package files
 import generate_2D_contour as gen_contour
 
+#----------------USER SPECIFIED PARAMETES/VARIABLES--------------------#
+N = 30                  # effective Shannon number input to localization2D.m
+Vmin_shell = 350        # in km/s
+rcond = 1e-4            # condition number of the matrix to be inverted (Moore-Penrose Inverse)
+
+#--------------PREVIOUS METHOD OF GOING BACK AND FORTH FROM MATLAB TO PYTHON TO GENERATE SLEPIANS----------------#
 '''
 N=50
 Slep_fname = f'localization2D-{N}.mat'
@@ -23,7 +29,9 @@ G, H, V, K, XYP, XY = Slepian_dict['G'], Slepian_dict['H'], Slepian_dict['V'],\
                       Slepian_dict['K'], Slepian_dict['XYP'], Slepian_dict['XY']
 '''
 
-[G, H, V, K, XYP, XY] = eng.localization2D('demo_VDF',nargout=6)
+#--------------CURRENT METHOD TO GENERATE SLEPIANS BY RUNING MATLAB FROM WITHIN PYTHON----------------#
+N = 30
+[G, H, V, K, XYP, XY] = eng.localization2D('demo_VDF', N, nargout=6)
 G = np.asarray(G)
 H = np.asarray(H)
 V = np.asarray(V)
@@ -36,31 +44,19 @@ XY = np.asarray(XY)
 # locating the Shannon number
 Nshannon = np.argmin(np.abs(V-0.5))
 
-# truncating the G to Shannon number
-# G = G[:,:,:Nshannon]
-
 # converting XYP from a flattened array into a meshgrid
 NX, NY, Nslep = G.shape
-YY, XX = np.reshape(XYP[:,1], (NX, NY), 'F'), np.reshape(XYP[:,0], (NX,NY), 'F')
+XX, YY =  np.reshape(XYP[:,0], (NX,NY), 'F'), np.reshape(XYP[:,1], (NX, NY), 'F')
 
-# plot limits
+# plotting limits
 xmin, xmax = XX.min(), XX.max()
 ymin, ymax = YY.min(), YY.max()
 
-plt.figure()
-plt.pcolormesh(XX, YY, G[:,:,1])
-plt.plot(XY[:,0], XY[:,1], '--k')
-plt.gca().set_aspect('equal')
-
-# loading the data
+# loading the data generated in the gyrotropization step
 VDF_2D = np.load('output_data_files/VDF_2D.npy')
 V1, V2 = np.load('output_data_files/V1.npy'), np.load('output_data_files/V2.npy')
 
-# igoring upto 6th energy shell
-# VDF_2D[:9,:] = np.nan
-VDF_2D = VDF_2D[8:,:]
-V1, V2 = V1[8:,:], V2[8:,:]
-
+# plotting the 2D raw VDF from gyrotropization step
 fig, ax = plt.subplots(2, 2, figsize=(8, 9), sharex=True, sharey=True)
 # ax[0,0].contourf(V1, V2, VDF_2D, vmin=0, vmax=6, levels=15)
 ax[0,0].pcolormesh(V1, V2, VDF_2D, vmin=0, vmax=6, cmap='hot', rasterized=True)
@@ -75,12 +71,11 @@ ax[0,0].set_title('VDF 2D raw')
 # interpolating the 2D raw VDF into the Slepian domain
 VDF_interp = griddata((np.ravel(V1,'F'), np.ravel(V2,'F')), np.ravel(VDF_2D, 'F'), (XX, YY), method='linear')
 
-# # setting shells under 313 to nan
-nan_mask_data = np.sqrt(XX**2 + YY**2) < 350
+# # setting shells under 350 km/s to nan
+nan_mask_data = np.sqrt(XX**2 + YY**2) < Vmin_shell
 VDF_interp[nan_mask_data] = np.nan
 
 # plotting the interpolated VDF
-# ax[0,1].contourf(XX, YY, VDF_interp, vmin=0, vmax=6, levels=15)
 ax[0,1].pcolormesh(XX, YY, VDF_interp, vmin=0, vmax=6, cmap='hot', rasterized=True)
 ax[0,1].contour(XX, YY, VDF_interp, levels=10, cmap='hot')
 ax[0,1].plot(XY[:,0], XY[:,1], '--k')
@@ -96,13 +91,10 @@ G_nonan = G[~nan_mask,:]
 M = G_nonan.T @ G_nonan
 __, S, __ = np.linalg.svd(M)
 I = np.identity(M.shape[0])
-coeffs = np.linalg.inv(M +  S.max() * 1e-4 * I) @ G_nonan.T @ VDF_interp[~nan_mask]
-# coeffs = np.linalg.pinv(M, rcond=1e-5) @ G_nonan.T @ VDF_interp[~nan_mask]
+coeffs = np.linalg.inv(M +  S.max() * rcond * I) @ G_nonan.T @ VDF_interp[~nan_mask]
 VDF_Sleprec = np.dot(G, coeffs)
 
-
 # plotting the reconstructed distribution
-# ax[1,0].contourf(XX, YY, VDF_Sleprec, vmin=0, vmax=6, levels=15)
 ax[1,0].pcolormesh(XX, YY, VDF_Sleprec, vmin=0, vmax=6, cmap='hot', rasterized=True)
 ax[1,0].contour(XX, YY, VDF_Sleprec, levels=10, cmap='hot')
 ax[1,0].plot(XY[:,0], XY[:,1], '--k')
@@ -112,7 +104,7 @@ ax[1,0].set_ylim([ymin, ymax])
 ax[1,0].set_aspect('equal')
 ax[1,0].set_title('Cartesian Slepian reconstruction')
 
-
+# plotting the reconstructed distribution vs. the raw 2D VDF from gyrotropization
 ax[1,1].pcolormesh(V1, V2, VDF_2D, vmin=0, vmax=6, cmap='hot', rasterized=True)
 ax[1,1].pcolormesh(XX[81:], YY[81:], VDF_Sleprec[81:], vmin=0, vmax=6, cmap='hot', rasterized=True)
 ax[1,1].set_xlim([xmin, xmax])
