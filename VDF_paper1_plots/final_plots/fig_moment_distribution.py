4e1bac79149dc57f3ee0e768d1f8fcea87a55963
srijaniiserprinceton <sbdas@princeton.edu>
2025-01-08 02:10:02 +0530

    Minor changes in plotting routines.

--- VDF_paper1_plots/final_plots/fig_moment_distribution.py
+++ VDF_paper1_plots/final_plots/fig_moment_distribution.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     t_tens_ratio = rec_temp / data_temp
     t_trace_ratio = np.array([np.trace(rec_temp[i]) for i in rec_moments.keys()])/np.array([np.trace(data_temp[i]) for i in data_moments.keys()])
 
-    fig, ax = plt.subplots(1,3, figsize=(12,4.5), sharex=True, sharey=True)
+    fig, ax = plt.subplots(1,3, figsize=(12,3), sharex=True, sharey=True)
 
     nbins = 15
     lmin = 0.9
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     dflag = True
 
     ax[0].hist(den_ratio, bins=nbins, range=(lmin, lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$n_{rec}/n_{data} = $'+f'${np.round(np.quantile(den_ratio, 0.5), 3)}^{{+{np.round(np.quantile(den_ratio - np.quantile(den_ratio, 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(den_ratio, 0.14) - np.quantile(den_ratio, 0.5), 4)}}}$')
-    ax[0].legend(frameon=False, fontsize=12)
+    ax[0].legend(frameon=False, fontsize=9)
     ax[0].set_xlabel(r'Density Ratio', fontsize=14)
     ax[0].set_ylim([0,100])
 
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     ax[1].hist(vvec_ratio[:,0], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{x,rec}/V_{x,data}$ = '+f'${np.round(np.quantile(vvec_ratio[:,0], 0.5), 3)}^{{+{np.round(np.quantile(vvec_ratio[:,0] - np.quantile(vvec_ratio[:,0], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(vvec_ratio[:,0], 0.14) - np.quantile(vvec_ratio[:,0], 0.5), 4)}}}$', color='k')
     ax[1].hist(vvec_ratio[:,1], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{y,rec}/V_{y,data}$ = '+f'${np.round(np.quantile(vvec_ratio[:,0], 0.5), 3)}^{{+{np.round(np.quantile(vvec_ratio[:,1] - np.quantile(vvec_ratio[:,1], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(vvec_ratio[:,1], 0.14) - np.quantile(vvec_ratio[:,1], 0.5), 4)}}}$', color='tab:orange')
     ax[1].hist(vvec_ratio[:,2], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{z,rec}/V_{z,data}$ = '+f'${np.round(np.quantile(vvec_ratio[:,0], 0.5), 3)}^{{+{np.round(np.quantile(vvec_ratio[:,2] - np.quantile(vvec_ratio[:,2], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(vvec_ratio[:,2], 0.14) - np.quantile(vvec_ratio[:,2], 0.5), 4)}}}$', color='tab:green')
-    ax[1].legend(frameon=False, fontsize=12)
+    ax[1].legend(frameon=False, fontsize=9)
     ax[1].set_xlabel(r'Velocity Ratios', fontsize=14)
 
     ax[2].hist(t_trace_ratio, bins=nbins, range=(lmin, lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$Tr\{T_{rec}\}/Tr\{T_{data}\}$ = '+f'${np.round(np.quantile(t_trace_ratio, 0.5), 3)}^{{+{np.round(np.quantile(t_trace_ratio - np.quantile(t_trace_ratio, 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(t_trace_ratio, 0.14) - np.quantile(t_trace_ratio, 0.5), 4)}}}$', color='tab:blue')
@@ -62,9 +62,9 @@ if __name__ == "__main__":
     ax[2].hist(t_tens_ratio[:,2,2], bins=nbins, range=(lmin, lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$T_{zz,rec}/T_{zz,data}$ = '+f'${np.round(np.quantile(t_tens_ratio[:,2,2], 0.5), 3)}^{{+{np.round(np.quantile(t_tens_ratio[:,2,2] - np.quantile(t_tens_ratio[:,2,2], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(t_tens_ratio[:,2,2], 0.14) - np.quantile(t_tens_ratio[:,2,2], 0.5), 4)}}}$', color='tab:green')
     ax[2].legend()
     ax[2].set_xlabel(r'Temperature Ratios', fontsize=14)
-    ax[2].legend(frameon=False, fontsize=12)
+    ax[2].legend(frameon=False, fontsize=9)
 
-    [ax[i].set_box_aspect(1) for i in range(3)]
+    # [ax[i].set_box_aspect(1) for i in range(3)]
 
     # ax[0].set_xlabel(r'$n_{rec}/n_{orig}$', fontsize=14)
     # ax[1].set_xlabel(r'$v_{rec}/v_{orig}$', fontsize=14)
@@ -85,10 +85,10 @@ if __name__ == "__main__":
     #     # axs.text(0.04, 0.8, f'{moment_label[ax_idx]}', transform=axs.transAxes,
     #             # va='bottom', ha='left', color='black', fontsize=20, fontweight='bold')
 
-    fig.text(0.55, 0.04, r'Moment Ratios', ha='center', fontsize=16, fontweight='bold')
-    fig.text(0.01, 0.5, r'Probability Density', va='center', rotation='vertical', fontsize=16, fontweight='bold')
+    # fig.text(0.55, 0.04, r'Moment Ratios', ha='center', fontsize=16, fontweight='bold')
+    fig.text(0.01, 0.55, r'Probability Density', va='center', rotation='vertical', fontsize=14)#, fontweight='bold')
 
     # plt.subplots_adjust(left=0.06, right=0.98, wspace=0.35, top=1.0, bottom=0.1)
     plt.subplots_adjust(left=0.08, right=0.97, hspace=0.05, wspace=0.05, top=0.95, bottom=0.2)
 
-    plt.savefig('./VDF_paper1_plots/moment_rec_distribution.pdf')
+    plt.savefig('./moment_rec_distribution.pdf')
