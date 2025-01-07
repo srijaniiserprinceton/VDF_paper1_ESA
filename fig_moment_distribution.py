8f30ed6f0503218485924483daa94e4a42ac9fe7
TerresM <michael.terres@cfa.harvard.edu>
2025-01-07 11:35:16 -0500

    fig_moment_distribution.py update

--- fig_moment_distribution.py
+++ fig_moment_distribution.py
@@ -42,24 +42,24 @@ if __name__ == "__main__":
     htype = 'step'
     dflag = True
 
-    ax[0].hist(den_ratio, bins=nbins, range=(lmin, lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$n_{rec}/n_{data} = $'+f'{np.round(np.mean(den_ratio), 3)}'+r'$\pm$'+f'{np.round(np.var(den_ratio), 4)}')
+    ax[0].hist(den_ratio, bins=nbins, range=(lmin, lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$n_{rec}/n_{data} = $'+f'${np.round(np.quantile(den_ratio, 0.5), 3)}^{{+{np.round(np.quantile(den_ratio - np.quantile(den_ratio, 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(den_ratio, 0.14) - np.quantile(den_ratio, 0.5), 4)}}}$')
     ax[0].legend(frameon=False, fontsize=12)
     ax[0].set_xlabel(r'Density Ratio', fontsize=14)
     ax[0].set_ylim([0,100])
 
-    ax[1].hist(vmag_ratio, bins=nbins, range=(lmin,lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$|V_{rec}|/|V_{data}|$ = '+f'{np.round(np.mean(vmag_ratio), 3)}'+r'$\pm$'+f'{np.round(np.var(vmag_ratio), 4)}', color='tab:blue')
+    ax[1].hist(vmag_ratio, bins=nbins, range=(lmin,lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$|V_{rec}|/|V_{data}|$ = '+f'${np.round(np.quantile(vmag_ratio, 0.5), 3)}^{{+{np.round(np.quantile(vmag_ratio - np.quantile(vmag_ratio, 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(vmag_ratio, 0.14) - np.quantile(vmag_ratio, 0.5), 4)}}}$', color='tab:blue')
 
-    ax[1].hist(vvec_ratio[:,0], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{x,rec}/V_{x,data}$ = '+f'{np.round(np.mean(vvec_ratio[:,0]), 3)}'+r'$\pm$'+f'{np.round(np.var(vvec_ratio[:,0]), 4)}', color='k')
-    ax[1].hist(vvec_ratio[:,1], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{y,rec}/V_{y,data}$ = '+f'{np.round(np.mean(vvec_ratio[:,1]), 3)}'+r'$\pm$'+f'{np.round(np.var(vvec_ratio[:,1]), 4)}', color='tab:orange')
-    ax[1].hist(vvec_ratio[:,2], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{z,rec}/V_{z,data}$ = '+f'{np.round(np.mean(vvec_ratio[:,2]), 3)}'+r'$\pm$'+f'{np.round(np.var(vvec_ratio[:,2]), 4)}', color='tab:green')
+    ax[1].hist(vvec_ratio[:,0], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{x,rec}/V_{x,data}$ = '+f'${np.round(np.quantile(vvec_ratio[:,0], 0.5), 3)}^{{+{np.round(np.quantile(vvec_ratio[:,0] - np.quantile(vvec_ratio[:,0], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(vvec_ratio[:,0], 0.14) - np.quantile(vvec_ratio[:,0], 0.5), 4)}}}$', color='k')
+    ax[1].hist(vvec_ratio[:,1], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{y,rec}/V_{y,data}$ = '+f'${np.round(np.quantile(vvec_ratio[:,0], 0.5), 3)}^{{+{np.round(np.quantile(vvec_ratio[:,1] - np.quantile(vvec_ratio[:,1], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(vvec_ratio[:,1], 0.14) - np.quantile(vvec_ratio[:,1], 0.5), 4)}}}$', color='tab:orange')
+    ax[1].hist(vvec_ratio[:,2], bins=nbins, range=(lmin,lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$V_{z,rec}/V_{z,data}$ = '+f'${np.round(np.quantile(vvec_ratio[:,0], 0.5), 3)}^{{+{np.round(np.quantile(vvec_ratio[:,2] - np.quantile(vvec_ratio[:,2], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(vvec_ratio[:,2], 0.14) - np.quantile(vvec_ratio[:,2], 0.5), 4)}}}$', color='tab:green')
     ax[1].legend(frameon=False, fontsize=12)
     ax[1].set_xlabel(r'Velocity Ratios', fontsize=14)
 
-    ax[2].hist(t_trace_ratio, bins=nbins, range=(lmin, lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$Tr\{T_{rec}\}/Tr\{T_{data}\}$ = '+f'{np.round(np.mean(t_trace_ratio), 3)}'+r'$\pm$'+f'{np.round(np.var(t_trace_ratio), 4)}', color='tab:blue')
+    ax[2].hist(t_trace_ratio, bins=nbins, range=(lmin, lmax), histtype='bar', density=dflag, alpha=0.5, label=r'$Tr\{T_{rec}\}/Tr\{T_{data}\}$ = '+f'${np.round(np.quantile(t_trace_ratio, 0.5), 3)}^{{+{np.round(np.quantile(t_trace_ratio - np.quantile(t_trace_ratio, 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(t_trace_ratio, 0.14) - np.quantile(t_trace_ratio, 0.5), 4)}}}$', color='tab:blue')
 
-    ax[2].hist(t_tens_ratio[:,0,0], bins=nbins, range=(lmin, lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$T_{xx,rec}/T_{xx,data}$ = '+f'{np.round(np.mean(t_tens_ratio[:,0,0]), 3)}'+r'$\pm$'+f'{np.round(np.var(t_tens_ratio[:,0,0]), 4)}', color='k')
-    ax[2].hist(t_tens_ratio[:,1,1], bins=nbins, range=(lmin, lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$T_{yy,rec}/T_{yy,data}$ = '+f'{np.round(np.mean(t_tens_ratio[:,1,1]), 3)}'+r'$\pm$'+f'{np.round(np.var(t_tens_ratio[:,1,1]), 4)}', color='tab:orange')
-    ax[2].hist(t_tens_ratio[:,2,2], bins=nbins, range=(lmin, lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$T_{zz,rec}/T_{zz,data}$ = '+f'{np.round(np.mean(t_tens_ratio[:,2,2]), 3)}'+r'$\pm$'+f'{np.round(np.var(t_tens_ratio[:,2,2]), 4)}', color='tab:green')
+    ax[2].hist(t_tens_ratio[:,0,0], bins=nbins, range=(lmin, lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$T_{xx,rec}/T_{xx,data}$ = '+f'${np.round(np.quantile(t_tens_ratio[:,0,0], 0.5), 3)}^{{+{np.round(np.quantile(t_tens_ratio[:,0,0] - np.quantile(t_tens_ratio[:,0,0], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(t_tens_ratio[:,0,0], 0.14) - np.quantile(t_tens_ratio[:,0,0], 0.5), 4)}}}$', color='k')
+    ax[2].hist(t_tens_ratio[:,1,1], bins=nbins, range=(lmin, lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$T_{yy,rec}/T_{yy,data}$ = '+f'${np.round(np.quantile(t_tens_ratio[:,1,1], 0.5), 3)}^{{+{np.round(np.quantile(t_tens_ratio[:,1,1] - np.quantile(t_tens_ratio[:,1,1], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(t_tens_ratio[:,1,1], 0.14) - np.quantile(t_tens_ratio[:,1,1], 0.5), 4)}}}$', color='tab:orange')
+    ax[2].hist(t_tens_ratio[:,2,2], bins=nbins, range=(lmin, lmax), histtype=htype, density=dflag, alpha=1, linewidth=2, label=r'$T_{zz,rec}/T_{zz,data}$ = '+f'${np.round(np.quantile(t_tens_ratio[:,2,2], 0.5), 3)}^{{+{np.round(np.quantile(t_tens_ratio[:,2,2] - np.quantile(t_tens_ratio[:,2,2], 0.5), 0.86), 4)}}}_{{{np.round(np.quantile(t_tens_ratio[:,2,2], 0.14) - np.quantile(t_tens_ratio[:,2,2], 0.5), 4)}}}$', color='tab:green')
     ax[2].legend()
     ax[2].set_xlabel(r'Temperature Ratios', fontsize=14)
     ax[2].legend(frameon=False, fontsize=12)
